9c8aa1e298ab62c50164c0505e6c362cc0b826fe
Sabin <sabintheworld@gmail.com>
2025-04-15 10:52:02 -0700

    Adds initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <main className="bg-white rounded-2xl w-full h-full min-h-screen">
+        <h2>Hello World</h2>
+    </main>
+  );
+}
+
+export default App;

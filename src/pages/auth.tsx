740831c05873af8d9db2c4c3d6a79546d6d29c51
Sabin <sabintheworld@gmail.com>
2025-04-16 16:36:44 -0700

    Standard colors

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -109,7 +109,7 @@ export default function AuthPage() {
 
   return (
     <div className="flex-1 flex items-center justify-center p-4">
-      <Card className="w-full max-w-md bg-white">
+      <Card className="w-full max-w-md bg-zinc-50">
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl text-center text-zinc-900">
             {isSignIn ? 'Sign In' : 'Sign Up'}
@@ -128,7 +128,7 @@ export default function AuthPage() {
                 type="button"
                 disabled={!!oauthLoading}
                 onClick={() => handleOAuthSignIn('oauth_github')}
-                className="w-full bg-white hover:bg-gray-50"
+                className="w-full bg-zinc-50 hover:bg-gray-50"
               >
                 <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
                   <path fill="currentColor" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" />
@@ -140,7 +140,7 @@ export default function AuthPage() {
                 type="button"
                 disabled={!!oauthLoading}
                 onClick={() => handleOAuthSignIn('oauth_google')}
-                className="w-full bg-white hover:bg-gray-50"
+                className="w-full bg-zinc-50 hover:bg-gray-50"
               >
                 <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
                   <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
@@ -156,7 +156,7 @@ export default function AuthPage() {
                 <span className="w-full border-t border-zinc-200" />
               </div>
               <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-white px-2 text-zinc-500">
+                <span className="bg-zinc-50 px-2 text-zinc-500">
                   Or continue with email
                 </span>
               </div>
